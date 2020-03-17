1ca718e7d222fc53ebbf5951e0ef0aa2dc5bb8d3
Dr Nic Williams <drnicwilliams@gmail.com>
2020-03-17 13:26:34 +1000

    focus on updating rubygems; bumping/testing ruby itself harder as requires generating buildkite step

--- .buildkite/scripts/update-dependencies.sh
+++ .buildkite/scripts/update-dependencies.sh
@@ -5,9 +5,14 @@ set -euo pipefail
 [[ -n "${UPDATE_RUBY_VERSION:-}" ]] && {
   echo "--- Update :ruby:"
   sed -i "s/^ruby.*$/ruby \"$UPDATE_RUBY_VERSION\"/g" Gemfile
-  cat Gemfile
 }
 
+[[ -n "${UPDATE_RUBYGEM_VERSION_SINATRA:-}" ]] && {
+  echo "--- Update :ruby: sinatra ${UPDATE_RUBYGEM_VERSION_SINATRA}"
+  sed -i "s/^gem \"sinatra\".*$/gem \"sinatra\", \"$UPDATE_RUBYGEM_VERSION_SINATRA\"/g" Gemfile
+}
+
+cat Gemfile
 echo "--- Bundle :ruby:"
 gem install bundler
 bundle install --verbose
